caac76e84ef47612eb2c4f19544556fdf4624f5b
AlbertTB <albert.turonb@gmail.com>
2024-10-30 21:19:29 +0100

    prints whole file if  <= 2*number of lines

--- peek.sh
+++ peek.sh
@@ -1,3 +1,14 @@
-if [[ -n $2 ]]; then head -n 3 $1; echo ...; tail -n 3 $1; else head -n $2 $1; echo ...; tail -n $2 $1; fi
+X=$2
+linecount=$(cat $1 | wc -l) 
 
-
+if (( linecount <= 2 * X )); then
+    cat "$1"
+elif [[ -n $2 ]]; then
+    head -n 3 "$1"
+    echo "..."
+    tail -n 3 "$1"
+else
+    head -n "$2" "$1"
+    echo "..."
+    tail -n "$2" "$1"
+fi
